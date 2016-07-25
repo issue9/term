5f7d5904b1db0eece8bb7fe839b75dab506154fe
caixw <xnotepad@gmail.com>
2016-07-25 22:08:27 +0800

    修正 fmt_posix.go 调用错误

--- colors/fmt_posix.go
+++ colors/fmt_posix.go
@@ -6,10 +6,7 @@
 
 package colors
 
-import (
-	"io"
-	"os"
-)
+import "io"
 
 // Fprint 带色彩输出的 fmt.Fprint，颜色值被转换成 ANSI 码一起写入到 w 中。
 func Fprint(w io.Writer, foreground, background Color, v ...interface{}) (int, error) {
@@ -23,22 +20,22 @@ func Fprintln(w io.Writer, foreground, background Color, v ...interface{}) (int,
 
 // Fprintf 带色彩输出的 fmt.Fprintf，颜色值被转换成 ANSI 码一起写入到 w 中。
 func Fprintf(w io.Writer, foreground, background Color, format string, v ...interface{}) (int, error) {
-	return fprint(w, foreground, background, format, v...)
+	return fprintf(w, foreground, background, format, v...)
 }
 
 // Print 带色彩输出的 fmt.Print，输出到 os.Stdout。
 func Print(foreground, background Color, v ...interface{}) (int, error) {
-	return print(os.Stdout, foreground, background, v...)
+	return print(foreground, background, v...)
 }
 
 // Println 带色彩输出的 fmt.Println，输出到 os.Stdout。
 func Println(foreground, background Color, v ...interface{}) (int, error) {
-	return println(os.Stdout, foreground, background, v...)
+	return println(foreground, background, v...)
 }
 
 // Printf 带色彩输出的 fmt.Printf，输出到 os.Stdout。
 func Printf(foreground, background Color, format string, v ...interface{}) (int, error) {
-	return printf(os.Stdout, foreground, background, format, v...)
+	return printf(foreground, background, format, v...)
 }
 
 // Sprint 带色彩输出的 fmt.Sprint，返回的字符，颜色值被转换成 ANSI 代码与字符中返回。
