b376ab3974f2b6c1be6f28e63ecf42a19657420f
caixw <xnotepad@gmail.com>
2019-02-26 21:32:25 +0800

    更新版本到 go1.12

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// Package term 各平台的终端处理工具
+package term
