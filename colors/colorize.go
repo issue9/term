f5cf89de4ef93d613c8d2231999159cdb688c5d8
caixw <xnotepad@gmail.com>
2020-05-30 14:13:25 +0800

    fix(colors): New 函数添加对 t 的判断

--- colors/colorize.go
+++ colors/colorize.go
@@ -17,10 +17,15 @@ type Colorize struct {
 	Foreground Color
 	Background Color
 	sgr        string
+	reset      string
 }
 
 // New 新建一个 Colorize
 func New(t Type, foreground, background Color) Colorize {
+	if !isValidType(t) {
+		panic("无效的参数 t")
+	}
+
 	codes := make([]int, 0, 10)
 	if t != Normal {
 		codes = append(codes, int(t))
@@ -33,41 +38,51 @@ func New(t Type, foreground, background Color) Colorize {
 		Foreground: foreground,
 		Background: background,
 		sgr:        string(ansi.SGR(codes...)),
+		reset:      string(ansi.SGR(ansi.ResetCode)),
 	}
 }
 
 // Print 等同于 fmt.Print()
 func (c Colorize) Print(v ...interface{}) (int, error) {
-	return fmt.Print(c.sgr + fmt.Sprint(v...))
+	return fmt.Print(c.sgr, fmt.Sprint(v...), c.reset)
 }
 
 // Println 等同于 fmt.Println()
 func (c Colorize) Println(v ...interface{}) (int, error) {
-	return fmt.Println(c.sgr + fmt.Sprint(v...))
+	return fmt.Println(c.sgr, fmt.Sprint(v...), c.reset)
 }
 
 // Printf 等同于 fmt.Printf()
 func (c Colorize) Printf(format string, v ...interface{}) (int, error) {
-	return fmt.Printf(c.sgr + fmt.Sprintf(format, v...))
+	return fmt.Print(c.sgr, fmt.Sprintf(format, v...), c.reset)
 }
 
 // Fprint 等同于 fmt.Fprint()
 //
 // 若 w 不指向控制台，则颜色值被忽略。
 func (c Colorize) Fprint(w io.Writer, v ...interface{}) (int, error) {
-	return fmt.Fprint(w, c.sgr+fmt.Sprint(v...))
+	if !isConsole(w) {
+		return fmt.Fprint(w, v...)
+	}
+	return fmt.Fprint(w, c.sgr, fmt.Sprint(v...), c.reset)
 }
 
 // Fprintln 等同于 fmt.Fprintln()
 //
 // 若 w 不指向控制台，则颜色值被忽略。
 func (c Colorize) Fprintln(w io.Writer, v ...interface{}) (int, error) {
-	return fmt.Fprintln(w, c.sgr+fmt.Sprint(v...))
+	if !isConsole(w) {
+		return fmt.Fprintln(w, v...)
+	}
+	return fmt.Fprintln(w, c.sgr, fmt.Sprint(v...), c.reset)
 }
 
 // Fprintf 等同于 fmt.Fprintf()
 //
 // 若 w 不指向控制台，则颜色值被忽略。
 func (c Colorize) Fprintf(w io.Writer, format string, v ...interface{}) (int, error) {
-	return fmt.Fprintf(w, c.sgr+fmt.Sprintf(format, v...))
+	if !isConsole(w) {
+		return fmt.Fprintf(w, format, v...)
+	}
+	return fmt.Fprint(w, c.sgr, fmt.Sprintf(format, v...), c.reset)
 }
--- colors/colorize_test.go
+++ colors/colorize_test.go
@@ -3,7 +3,9 @@
 package colors
 
 import (
+	"bytes"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/issue9/assert"
@@ -12,12 +14,54 @@ import (
 func TestColorize(t *testing.T) {
 	a := assert.New(t)
 
-	for b := Default; b < maxNamedColor; b++ {
-		for f := Default; f < maxNamedColor; f++ {
-			c := New(Italic, f, b)
-			_, err := c.Printf("%s:%s\t", b.String(), f.String())
+	a.Panic(func() {
+		New(-100, Red, Red)
+	})
+
+	buf := new(bytes.Buffer)
+	c := New(Normal, Red, Blue)
+	_, err := c.Fprintln(buf, "test")
+	a.NotError(err).
+		Equal(buf.String(), "test\n")
+
+	// named colors
+	for fColor := Default; fColor < maxNamedColor; fColor++ {
+		fmt.Printf("foreground:%s\n", fColor)
+		for bColor := Default; bColor < maxNamedColor; bColor++ {
+			c := New(Italic, fColor, bColor)
+			_, err := c.Printf("%s\t", bColor.String())
 			a.NotError(err)
 		}
 		fmt.Println()
+		fmt.Println()
 	}
+
+	// 256
+	fmt.Printf("\n\n256 colors\n")
+	for i := maxNamedColor; i < end256Color; i++ {
+		_, err := (New(Bold, Color(i), Default)).Fprintf(os.Stdout, "%d\t", i)
+		a.NotError(err)
+	}
+	fmt.Println()
+
+	// RGB
+	fmt.Printf("\n\nRGB\n")
+	var b int
+	r := end256Color - 1
+	for i := 0; i < end256Color; i++ {
+		b = i + 5
+		if b >= end256Color {
+			b = 1
+		}
+
+		r -= 5
+		if r <= 0 {
+			r = end256Color - 1
+		}
+
+		rgb := RGB(uint8(r), uint8(end256Color-i), uint8(b))
+		_, err := (New(Italic, rgb, Default)).Printf("%s\t", rgb)
+		a.NotError(err)
+	}
+	fmt.Println()
 }
