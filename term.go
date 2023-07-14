a5f86364bfb59ab7e67647b91c85167a29f6f3f9
caixw <xnotepad@gmail.com>
2023-07-14 14:58:10 +0800

    feat: 添加 Size

---
+++ term.go
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+//go:build darwin || dragonfly || freebsd || hurd || linux || netbsd || openbsd
+
+package term
+
+import "golang.org/x/sys/unix"
+
+// Size 获取终端的尺寸
+func Size(h int) (width, height int, err error) {
+	size, err := unix.IoctlGetWinsize(h, unix.TIOCGWINSZ)
+	if err != nil {
+		return 0, 0, err
+	}
+	return int(size.Col), int(size.Row), nil
+}
---
+++ term_windows.go
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+package term
+
+import "golang.org/x/sys/windows"
+
+func Size(h winndows.Handle) (width, height int, err error) {
+	var info windows.ConsoleScreenBufferInfo
+	if err := windows.GetConsoleScreenBufferInfo(h, &info); err != nil {
+		return 0, 0, err
+	}
+	return int(info.Window.Right - info.Window.Left + 1), int(info.Window.Bottom - info.Window.Top + 1), nil
+}
